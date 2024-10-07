62fda40d35afaab20c304848ec9c14ff3a7bf9fb
dido-me <villantoyesteban@gmail.com>
2024-10-07 10:21:27 -0500

    fix: google ads

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,32 @@ export default defineConfig({
     react(),
     partytown({
       config: {
-        forward: ['dataLayer.push']
+        debug: false,
+        logCalls: false,
+        logGetters: false,
+        logSetters: false,
+        logImageRequests: false,
+        logScriptExecution: false,
+        logStackTraces: false,
+        forward: [
+          ['dataLayer.push']
+        ],
+        resolveUrl: (url) => {
+          const siteUrl = 'https://www.efsystemas.net/'
+          const proxyUrl = new URL(siteUrl)
+          if (
+            url.hostname === 'googleads.g.doubleclick.net' ||
+            url.hostname === 'www.googleadservices.com' ||
+            url.hostname === 'googletagmanager.com' ||
+            url.hostname === 'www.googletagmanager.com' ||
+            url.hostname === 'region1.google-analytics.com' ||
+            url.hostname === 'google.com'
+          ) {
+            proxyUrl.searchParams.append('apiurl', url.href)
+            return proxyUrl
+          }
+          return url
+        }
       }
     })],
   site: 'https://www.servidorperu.net'
